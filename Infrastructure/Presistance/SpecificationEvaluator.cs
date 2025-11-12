68662d99d7771b84a5ad8535ea8b7b9afebd158a
Ahmed-Abdulrahim <hemo089@outlook.com>
2025-11-12 23:39:41 +0200

    Add SpecificationEvaluator for query filtering and includes

---
+++ Infrastructure/Presistance/SpecificationEvaluator.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Contracts;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presistance
+{
+    static class SpecificationEvaluator
+    {
+        public static IQueryable<TEntity> GetQuery<TEntity, Tkey>(IQueryable<TEntity> inputQuery, ISpeicifications<TEntity, Tkey> spec)
+            where TEntity : BaseEntity<Tkey>
+        {
+            var query = inputQuery;
+            if (spec.Criteria is not null)
+            {
+                query = query.Where(spec.Criteria);
+            }
+            spec.AddInclude.Aggregate(query, (current, include) => current.Include(include));
+
+            return query;
+        }
+    }
+}

0895d78ebabd5b7527a79bba7116731ba7d806de
Ahmed-Abdulrahim <hemo089@outlook.com>
2025-12-13 17:04:42 +0200

    Add OrderProfile to Manage Operation between Dto And Base Classes

---
+++ StoreHub.Application/MappingProfile/OrderProfile.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using StoreHub.Application.Dtos.OrderDto;
+using StoreHub.Core.Models.Orders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreHub.Application.MappingProfile
+{
+    public class OrderProfile : Profile
+    {
+        public OrderProfile()
+        {
+            CreateMap<OrderItem, OrderItemDto>()
+                .ForMember(d => d.ProductId, o => o.MapFrom(s => s.Product.ProductId))
+                .ForMember(d => d.ProductName, o => o.MapFrom(s => s.Product.ProductName))
+                .ForMember(d => d.PictureUrl, o => o.MapFrom(s => s.Product.PictureUrl)).ReverseMap();
+
+            CreateMap<Order, OrderResultDto>()
+                .ForMember(d => d.PaymentStatus, o => o.MapFrom(s => s.PaymentStatus.ToString()))
+                .ForMember(d => d.DeliveryMethod, o => o.MapFrom(s => s.DeliveryMethod.ShortName))
+                .ForMember(d => d.Total, o => o.MapFrom(s => s.SubTotal + s.DeliveryMethod.Cost)).ReverseMap();
+
+            CreateMap<DeliveryMethod, DeliveryMethodDto>().ReverseMap();
+
+            CreateMap<ShippingAddress, ShippingAddressDto>().ReverseMap();
+        }
+    }
+}
